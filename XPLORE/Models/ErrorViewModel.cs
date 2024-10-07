ba128405a1cea50b28b863f079f38ade78415f98
AJI <AJI@DESKTOP-HTH718A>
2024-10-07 10:22:23 -0700

    Add project files.

---
+++ XPLORE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace XPLORE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
